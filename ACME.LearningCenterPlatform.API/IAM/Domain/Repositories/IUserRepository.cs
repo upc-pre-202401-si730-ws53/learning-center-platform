5c5f2d087a760f6a9fac94360143564af642cf87
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-06 15:53:57 -0500

    feat(iam): added user aggregate outbound service contract.

---
+++ ACME.LearningCenterPlatform.API/IAM/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.IAM.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> FindByUsernameAsync(string username);
+    bool ExistsByUsername(string username);
+}
