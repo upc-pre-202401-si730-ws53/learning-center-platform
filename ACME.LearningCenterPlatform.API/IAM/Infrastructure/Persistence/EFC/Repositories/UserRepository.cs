13f34891d9b7b23a9d6a6fdafb24f8dec630d80d
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-06 16:48:19 -0500

    feat(iam): added user persistence outbound service implementation.

---
+++ ACME.LearningCenterPlatform.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.IAM.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.LearningCenterPlatform.API.IAM.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository 
+{
+    public async Task<User?> FindByUsernameAsync(string username)
+    {
+        return await Context.Set<User>().FirstOrDefaultAsync(user => user.Username.Equals(username));
+    }
+
+    public bool ExistsByUsername(string username)
+    {
+        return Context.Set<User>().Any(user => user.Username.Equals(username));
+    }
+}
--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,4 @@
+using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
 using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
@@ -87,6 +88,12 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
                 a.Property(s => s.PostalCode).HasColumnName("AddressPostalCode");
                 a.Property(s => s.Country).HasColumnName("AddressCountry");
             });
+        
+        // IAM Context
+        builder.Entity<User>().HasKey(u => u.Id);
+        builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(u => u.Username).IsRequired();
+        builder.Entity<User>().Property(u => u.PasswordHash).IsRequired();
         
         // Apply SnakeCase Naming Convention
         builder.UseSnakeCaseWithPluralizedTableNamingConvention();
